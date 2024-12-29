ddd4652de2c2125359d9a70c89003ce367b61a47
tanyf5 <leehmann05@gmail.com>
2024-12-29 01:34:58 +0100

    outdir

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
+      outDir: 'dist',
       emptyOutDir: true,
     },
     plugins: [
